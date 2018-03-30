bc4f58ee189e088b8e2253213d932110b75691db
Nuno Nunes <nunonunes@Nunos-iMac.local>
2018-03-30 23:54:29 +0100

    first

--- js/script.js
+++ js/script.js
@@ -1,22 +1,59 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    $("a").removeClass("active");
+    $(this).addClass("active");
 
-function scrollTo(to, duration) {
-    if (document.body.scrollTop == to) return;
-    var diff = to - document.body.scrollTop;
-    var scrollStep = Math.PI / (duration / 10);
-    var count = 0, currPos;
-    start = element.scrollTop;
-    scrollInterval = setInterval(function(){
-        if (document.body.scrollTop != to) {
-            count = count + 1;
-            currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
-            document.body.scrollTop = currPos;
-        }
-        else { clearInterval(scrollInterval); }
-    },10);
-}
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-function test(elID)
-{
-    var dest = document.getElementById(elID);
-    scrollTo(dest.offsetTop, 5000);
-}
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+
+
+  });
+});
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+
+    if(scroll >= $('#QuemSomos').offset().top-150){
+        $("a").removeClass("active");
+        $("#linkQuem").addClass("active");
+    }
+
+    if(scroll >= $('#Precos').offset().top -150){
+        $("a").removeClass("active");
+        $("#linkPrecos").addClass("active");
+    }
+
+    if(scroll >= $('#Servicos').offset().top -150){
+        $("a").removeClass("active");
+        $("#linkServicos").addClass("active");
+    }
+
+    if(scroll >= $('#Empresas').offset().top -150){
+        $("a").removeClass("active");
+        $("#linkEmpresas").addClass("active");
+    }
+
+    if ((scroll + $(window).height()) > $(document).height()-100){
+        $("a").removeClass("active");
+        $("#linkConctatos").addClass("active");
+    }
+});
+
+$(window).scroll();
